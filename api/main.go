ec5e01edc5218244c2c265073732cafaf4e8421c
tanaka.takurou <tanaka.takurou@icloud.com>
2020-10-08 08:42:12 +0900

    fix for aws-sdk-go-v2 v0.26.0

--- api/main.go
+++ api/main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 	"bytes"
 	"regexp"
+	"reflect"
 	"strings"
 	"context"
 	"encoding/json"
@@ -15,10 +16,11 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/transcribe"
-	"github.com/aws/aws-sdk-go-v2/service/s3/s3manager"
+	ttypes "github.com/aws/aws-sdk-go-v2/service/transcribe/types"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	stypes "github.com/aws/aws-sdk-go-v2/service/s3/types"
 )
 
 type APIResponse struct {
@@ -95,34 +97,36 @@ func startTranscription(ctx context.Context, filedata string)(string, error) {
 	}
 	contentType := "audio/mp3"
 	filename := t.Format(layout2) + ".mp3"
-	uploader := s3manager.NewUploader(cfg)
-	_, err = uploader.Upload(&s3manager.UploadInput{
-		ACL: s3.ObjectCannedACLPublicRead,
+	if s3Client == nil {
+		s3Client = getS3Client()
+	}
+	input := &s3.PutObjectInput{
+		ACL: stypes.ObjectCannedACLPublicRead,
 		Bucket: aws.String(os.Getenv("BUCKET_NAME")),
 		Key: aws.String(S3MediaPath + "/" + filename),
 		Body: bytes.NewReader(data),
 		ContentType: aws.String(contentType),
-	})
+	}
+	_, err = s3Client.PutObject(ctx, input)
 	if err != nil {
 		return "", err
 	}
 	url := "s3://" + os.Getenv("BUCKET_NAME") + "/" + S3MediaPath + "/" + filename
 
 	if transcribeClient == nil {
-		transcribeClient = transcribe.New(cfg)
+		transcribeClient = getTranscribeClient()
 	}
 
-	input := &transcribe.StartTranscriptionJobInput{
+	input_ := &transcribe.StartTranscriptionJobInput{
 		TranscriptionJobName: aws.String(filename),
-		LanguageCode: transcribe.LanguageCodeJaJp,
+		LanguageCode: ttypes.LanguageCodeJa_jp,
 		OutputBucketName: aws.String(os.Getenv("BUCKET_NAME")),
-		MediaFormat: transcribe.MediaFormatMp3,
-		Media: &transcribe.Media{
+		MediaFormat: ttypes.MediaFormatMp3,
+		Media: &ttypes.Media{
 			MediaFileUri: aws.String(url),
 		},
 	}
-	req := transcribeClient.StartTranscriptionJobRequest(input)
-	_, err = req.Send(ctx)
+	_, err = transcribeClient.StartTranscriptionJob(ctx, input_)
 	if err != nil {
 		return "", err
 	}
@@ -132,52 +136,51 @@ func startTranscription(ctx context.Context, filedata string)(string, error) {
 
 func checkProgress(ctx context.Context, jobName string)(string, error) {
 	if transcribeClient == nil {
-		transcribeClient = transcribe.New(cfg)
+		transcribeClient = getTranscribeClient()
 	}
 
 	input := &transcribe.ListTranscriptionJobsInput{
 		JobNameContains: aws.String(jobName),
 	}
-	req := transcribeClient.ListTranscriptionJobsRequest(input)
-	res, err := req.Send(ctx)
+	res, err := transcribeClient.ListTranscriptionJobs(ctx, input)
 	if err != nil {
 		return "", err
 	}
-	return string(res.ListTranscriptionJobsOutput.TranscriptionJobSummaries[0].TranscriptionJobStatus), nil
+	return string(res.TranscriptionJobSummaries[0].TranscriptionJobStatus), nil
 }
 
 func getTranscriptionJob(ctx context.Context, jobName string)(string, error) {
 	if transcribeClient == nil {
-		transcribeClient = transcribe.New(cfg)
+		transcribeClient = getTranscribeClient()
 	}
 
 	input := &transcribe.GetTranscriptionJobInput{
 		TranscriptionJobName: aws.String(jobName),
 	}
-	req := transcribeClient.GetTranscriptionJobRequest(input)
-	res, err := req.Send(ctx)
+	res, err := transcribeClient.GetTranscriptionJob(ctx, input)
 	if err != nil {
 		return "", err
 	}
-        url := aws.StringValue(res.GetTranscriptionJobOutput.TranscriptionJob.Transcript.TranscriptFileUri)
+	url := stringValue(res.TranscriptionJob.Transcript.TranscriptFileUri)
 	rep := regexp.MustCompile(`\s*/\s*`)
 	tmp := rep.Split(url, -1)
+	tmp_ := strings.Replace(tmp[len(tmp) - 1], "\"", "", 1)
 
 	if s3Client == nil {
-		s3Client = s3.New(cfg)
+		s3Client = getS3Client()
 	}
-	req2 := s3Client.GetObjectRequest(&s3.GetObjectInput{
+	input_ := &s3.GetObjectInput{
 		Bucket: aws.String(os.Getenv("BUCKET_NAME")),
-		Key:    aws.String(tmp[len(tmp) - 1]),
-	})
-	res2, err2 := req2.Send(ctx)
+		Key:    aws.String(tmp_),
+	}
+	res2, err2 := s3Client.GetObject(ctx, input_)
 	if err2 != nil {
 		return "", err2
 	}
-	defer res2.GetObjectOutput.Body.Close()
+	defer res2.Body.Close()
 
 	buf := new(bytes.Buffer)
-	buf.ReadFrom(res2.GetObjectOutput.Body)
+	buf.ReadFrom(res2.Body)
 	res_ := buf.String()
 
 	jsonBytes := ([]byte)(res_)
@@ -195,12 +198,94 @@ func getTranscriptionJob(ctx context.Context, jobName string)(string, error) {
 	return string(results_), nil
 }
 
-func init() {
+func getS3Client() *s3.Client {
+	if cfg.Region != os.Getenv("REGION") {
+		cfg = getConfig()
+	}
+	return s3.NewFromConfig(cfg)
+}
+
+func getTranscribeClient() *transcribe.Client {
+	if cfg.Region != os.Getenv("REGION") {
+		cfg = getConfig()
+	}
+	return transcribe.NewFromConfig(cfg)
+}
+
+func getConfig() aws.Config {
 	var err error
-	cfg, err = external.LoadDefaultAWSConfig()
-	cfg.Region = os.Getenv("REGION")
+	newConfig, err := config.LoadDefaultConfig()
+	newConfig.Region = os.Getenv("REGION")
 	if err != nil {
 		log.Print(err)
+	}
+	return newConfig
+}
+
+func stringValue(i interface{}) string {
+	var buf bytes.Buffer
+	strVal(reflect.ValueOf(i), 0, &buf)
+	return buf.String()
+}
+
+func strVal(v reflect.Value, indent int, buf *bytes.Buffer) {
+	for v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	switch v.Kind() {
+	case reflect.Struct:
+		buf.WriteString("{\n")
+		for i := 0; i < v.Type().NumField(); i++ {
+			ft := v.Type().Field(i)
+			fv := v.Field(i)
+			if ft.Name[0:1] == strings.ToLower(ft.Name[0:1]) {
+				continue // ignore unexported fields
+			}
+			if (fv.Kind() == reflect.Ptr || fv.Kind() == reflect.Slice) && fv.IsNil() {
+				continue // ignore unset fields
+			}
+			buf.WriteString(strings.Repeat(" ", indent+2))
+			buf.WriteString(ft.Name + ": ")
+			if tag := ft.Tag.Get("sensitive"); tag == "true" {
+				buf.WriteString("<sensitive>")
+			} else {
+				strVal(fv, indent+2, buf)
+			}
+			buf.WriteString(",\n")
+		}
+		buf.WriteString("\n" + strings.Repeat(" ", indent) + "}")
+	case reflect.Slice:
+		nl, id, id2 := "", "", ""
+		if v.Len() > 3 {
+			nl, id, id2 = "\n", strings.Repeat(" ", indent), strings.Repeat(" ", indent+2)
+		}
+		buf.WriteString("[" + nl)
+		for i := 0; i < v.Len(); i++ {
+			buf.WriteString(id2)
+			strVal(v.Index(i), indent+2, buf)
+			if i < v.Len()-1 {
+				buf.WriteString("," + nl)
+			}
+		}
+		buf.WriteString(nl + id + "]")
+	case reflect.Map:
+		buf.WriteString("{\n")
+		for i, k := range v.MapKeys() {
+			buf.WriteString(strings.Repeat(" ", indent+2))
+			buf.WriteString(k.String() + ": ")
+			strVal(v.MapIndex(k), indent+2, buf)
+			if i < v.Len()-1 {
+				buf.WriteString(",\n")
+			}
+		}
+		buf.WriteString("\n" + strings.Repeat(" ", indent) + "}")
+	default:
+		format := "%v"
+		switch v.Interface().(type) {
+		case string:
+			format = "%q"
+		}
+		fmt.Fprintf(buf, format, v.Interface())
 	}
 }
 
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/tanaka-takurou/serverless-message-board-go
+
+go 1.15
+
+require (
+	github.com/aws/aws-lambda-go v1.19.1
+	github.com/aws/aws-sdk-go-v2 v0.26.0
+	github.com/aws/aws-sdk-go-v2/config v0.1.1
+	github.com/aws/aws-sdk-go-v2/service/s3 v0.26.0
+	github.com/aws/aws-sdk-go-v2/service/transcribe v0.26.0
+)
